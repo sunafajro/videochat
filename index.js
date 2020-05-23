d69467898136eeb5d8bdb33cb5106cbf08091390
Evgeny Belkin <sunafajro@gmail.com>
2020-05-23 13:43:38 +0300

    Вывод видео и аудио текущего пользователя на страницу.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
+const port = process.env.PORT || "3000";
 
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile('./public/index.html');
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Example app listening on port 3000!');
 });
